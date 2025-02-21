8fd54e99ca06d36606a6fb52278c35cabc30991f
Blackjay07 <116186005+rpdistiso@users.noreply.github.com>
2025-02-21 14:50:35 -0500

    Create rpmfusion1.sh

---
+++ rpmfusion1.sh
@@ -0,0 +1,4 @@
+sudo sudo rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+ reboot
